c1ce5ee2bf0d0c8d738c1c17a2e495be6834710d
Joel Perez <perezjoeldario@gmail.com>
2023-10-03 09:51:25 -0300

    a

--- logica.mjs
+++ logica.mjs
@@ -1,22 +1,23 @@
 // logica.mjs
+
 import { traccar, meitrack, autoleaders } from "./protocolos.mjs";
 
-const meitrackResult = meitrack(data);
-const autoleadersResult = autoleaders(data);
-const traccarResult = traccar(data);
-
-let protocolType;
-if (data[0] === "$") {
-  protocolType = "meitrack";
-} else if (data[0] === "*") {
-  protocolType = "autoleaders";
-} else if (data[0] === "P") {
-  protocolType = "traccar";
-} else {
-  console.log("Protocolo desconocido");
-}
-
 export const handler = (data) => {
+  const meitrackResult = meitrack(data);
+  const autoleadersResult = autoleaders(data);
+  const traccarResult = traccar(data);
+
+  let protocolType;
+  if (data[0] === "$") {
+    protocolType = "meitrack";
+  } else if (data[0] === "*") {
+    protocolType = "autoleaders";
+  } else if (data[0] === "P") {
+    protocolType = "traccar";
+  } else {
+    return console.log("Protocolo desconocido");
+  }
+
   try {
     if (data === null || data === undefined) {
       return null;
@@ -38,4 +39,5 @@ export const handler = (data) => {
     return console.log("Error en el protocolo");
   }
 };
+
 export default handler;
