a657fa746ace1015a3b0e152907d885dce4a704e
Joel Perez <perezjoeldario@gmail.com>
2023-10-02 22:00:49 -0300

    a

--- logica.mjs
+++ logica.mjs
@@ -1,29 +1,35 @@
 import { traccar, meitrack, autoleaders } from "./protocolos.mjs";
-import data from "./servidor.mjs";
 
 const meitrackResult = meitrack(data);
 const autoleadersResult = autoleaders(data);
 const traccarResult = traccar(data);
 
 export const handler = (data) => {
-  if (data[0] === "$") {
-    try {
-      meitrackResult;
-    } catch (e) {
-      console.log(e);
+  try {
+    if (data === null || data === undefined) {
+      return null;
+    } else if (data[0] === "$") {
+      try {
+        meitrackResult;
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (data[0] === "*") {
+      try {
+        autoleadersResult;
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (data[0] === "P") {
+      try {
+        traccarResult;
+      } catch (e) {
+        console.log(e);
+      }
     }
-  } else if (data[0] === "*") {
-    try {
-      autoleadersResult;
-    } catch (e) {
-      console.log(e);
-    }
-  } else if (data[0] === "P") {
-    try {
-      traccarResult;
-    } catch (e) {
-      console.log(e);
-    }
+  } catch (e) {
+    console.log(e);
+    return null;
   }
 };
 export default handler;
